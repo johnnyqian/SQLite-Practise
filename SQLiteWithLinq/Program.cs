c947cd0010409b8e6756b939f9b2788c197cde40
Johnny Qian <jiaqia@microsoft.com>
2018-04-05 12:53:38 +0800

    Update project SQLiteWithLinq

--- SQLiteWithLinq/Program.cs
+++ SQLiteWithLinq/Program.cs
@@ -2,9 +2,12 @@
 // SQLite LINQ Provider Data Context, https://stackoverflow.com/questions/22561248/sqlite-linq-provider-data-context
 
 using System;
+using System.Configuration;
+using System.Data;
 using System.Data.Linq;
 using System.Data.Linq.Mapping;
 using System.Data.SQLite;
+using System.Linq;
 
 namespace SQLiteWithLinq
 {
@@ -12,7 +15,7 @@ namespace SQLiteWithLinq
     {
         static void Main()
         {
-            using (var dbConnection = new SQLiteConnection("Data Source=Database.sqlite;Version=3;"))
+            using (var dbConnection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SQLite"].ConnectionString))
             {
                 dbConnection.Open();
 
@@ -33,17 +36,36 @@ namespace SQLiteWithLinq
                 command = new SQLiteCommand(sql, dbConnection);
                 command.ExecuteNonQuery();
 
-                using (var context = new DataContext(dbConnection))
+                using (var context = new MyDataContext(dbConnection))
                 {
-                    var companies = context.GetTable<Company>();
+                    var companies = context.Companies;
                     foreach (var company in companies)
                     {
                         Console.WriteLine("Company: {0} {1}", company.Id, company.Seats);
                     }
+
+                    // this will generate sql statement that not compatible with sqlite, need to resolve later
+                    var query = companies.Where(c => c.Id > 1).OrderBy(c => c.Id).Skip(1).Take(1);
+                    Console.WriteLine(query.ToString());
+                    foreach (var company in query)
+                    {
+                        Console.WriteLine("Company: {0} {1}", company.Id, company.Seats);
+                    }
+
                     Console.ReadKey();
                 }
             }
         }
+    }
+
+    public class MyDataContext : DataContext
+    {
+        public MyDataContext(IDbConnection connection)
+            : base(connection)
+        {
+        }
+
+        public Table<Company> Companies => GetTable<Company>();
     }
 
     [Table(Name = "company")]
